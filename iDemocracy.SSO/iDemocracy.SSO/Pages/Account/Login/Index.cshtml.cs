abb4858be5295bf1abbc73ed8042694d43a9b4ec
Luis Avila <zluissigloxx@hotmail.com>
2023-11-12 13:39:49 -0500

    cambios gitignore

--- iDemocracy.SSO/iDemocracy.SSO/Pages/Account/Login/Index.cshtml.cs
+++ iDemocracy.SSO/iDemocracy.SSO/Pages/Account/Login/Index.cshtml.cs
@@ -27,18 +27,14 @@ public class Index : PageModel
     private readonly UserManager<ApplicationUser> _userManager;
 
     public Index(
-            IIdentityServerInteractionService interaction,
-            IClientStore clientStore,
-            IAuthenticationSchemeProvider schemeProvider,
-            IIdentityProviderStore identityProviderStore,
-            IEventService events,
-            UserManager<ApplicationUser> userManager,
-            SignInManager<ApplicationUser> signInManager)
-        //TestUserStore users = null)
+        IIdentityServerInteractionService interaction,
+        IClientStore clientStore,
+        IAuthenticationSchemeProvider schemeProvider,
+        IIdentityProviderStore identityProviderStore,
+        IEventService events,
+        UserManager<ApplicationUser> userManager,
+        SignInManager<ApplicationUser> signInManager)
     {
-        // this is where you would plug in your own custom identity management library (e.g. ASP.NET Identity)
-        //_users = users ?? throw new Exception("Please call 'AddTestUsers(TestUsers.Users)' on the IIdentityServerBuilder in Startup or remove the TestUserStore from the AccountController.");
-
         _interaction = interaction;
         _clientStore = clientStore;
         _schemeProvider = schemeProvider;
@@ -212,53 +208,6 @@ public class Index : PageModel
             await _events.RaiseAsync(new UserLoginFailureEvent(Input.Username, "invalid credentials",
                 clientId: context?.Client.ClientId));
             ModelState.AddModelError(string.Empty, LoginOptions.InvalidCredentialsErrorMessage);
-
-
-            // validate username/password against in-memory store
-            //if (_users.ValidateCredentials(Input.Username, Input.Password))
-            //{
-            //    var user = _users.FindByUsername(Input.Username);
-            //    await _events.RaiseAsync(new UserLoginSuccessEvent(user.Username, user.SubjectId, user.Username,
-            //        clientId: context?.Client.ClientId));
-
-            //    // only set explicit expiration here if user chooses "remember me". 
-            //    // otherwise we rely upon expiration configured in cookie middleware.
-            //    AuthenticationProperties props = null;
-            //    if (LoginOptions.AllowRememberLogin && Input.RememberLogin)
-            //        props = new AuthenticationProperties
-            //        {
-            //            IsPersistent = true,
-            //            ExpiresUtc = DateTimeOffset.UtcNow.Add(LoginOptions.RememberMeLoginDuration)
-            //        };
-            //    ;
-
-            //    // issue authentication cookie with subject ID and username
-            //    var isuser = new IdentityServerUser(user.SubjectId)
-            //    {
-            //        DisplayName = user.Username
-            //    };
-
-            //    await HttpContext.SignInAsync(isuser, props);
-
-            //    if (context != null)
-            //    {
-            //        if (context.IsNativeClient())
-            //            // The client is native, so this change in how to
-            //            // return the response is for better UX for the end user.
-            //            return this.LoadingPage(Input.ReturnUrl);
-
-            //        // we can trust model.ReturnUrl since GetAuthorizationContextAsync returned non-null
-            //        return Redirect(Input.ReturnUrl);
-            //    }
-
-            //    // request for a local page
-            //    if (Url.IsLocalUrl(Input.ReturnUrl))
-            //        return Redirect(Input.ReturnUrl);
-            //    if (string.IsNullOrEmpty(Input.ReturnUrl))
-            //        return Redirect("~/");
-            //    // user might have clicked on a malicious link - should be logged
-            //    throw new Exception("invalid return URL");
-            //}
         }
 
         // something went wrong, show form with error
